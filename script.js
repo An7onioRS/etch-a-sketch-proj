556699c21cb5ff55810c43af7d80e04aab64c3d0
Antonio Soprano <antoniosimeonov7@gmail.com>
2022-03-02 14:17:51 +0200

    Implementing two loops to create rows and elements and append them to the main container.

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+const gridSize = 16; // establish grid size as a variable in case of future change
+
 const body = document.querySelector('body');
 const mainContainer = document.createElement('div').classList.add('main-container');
 body.appendChild(mainContainer);
 
 const rowContainers = [];
-const rowElements = []
+const rowElements = [];
+
+for (i = 0; i < gridSize; i++) {
+    // The first loop creates a gridSize number of rows. The second loop creates a gridSize number of elements. The elements are then appended to each row. The row is then appended to the main-container
+    
+    const currentRow = document.createElement('div').classList.add('row-container');
+    
+    for (j = 0; j < gridSize; ) {
+        const currentElement = document.createElement('div').classList.add('row-element');
+        currentRow.appendChild(currentElement);
+    }
+
+    mainContainer.appendChild(currentRow);
+}
