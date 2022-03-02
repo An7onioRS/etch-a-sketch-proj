efdf1aedfc0602a1e47c2f92905e213e8fe9904e
Antonio Soprano <antoniosimeonov7@gmail.com>
2022-03-02 23:25:01 +0200

    Adding and using an image as an input for the .innerHTML of all elements

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const gridSize = 16; // establish grid size as a variable in case of future change
+const externalIMG = '<img src="images/square-png.png">'
 
-const body = document.querySelector('body');
-const mainContainer = document.createElement('div').classList.add('main-container');
-body.appendChild(mainContainer);
+let mainContainer = document.createElement('div');
+mainContainer.classList.add('main-container')
+document.body.appendChild(mainContainer);
 
 const rowContainers = [];
 const rowElements = [];
@@ -10,10 +11,13 @@ const rowElements = [];
 for (i = 0; i < gridSize; i++) {
     // The first loop creates a gridSize number of rows. The second loop creates a gridSize number of elements. The elements are then appended to each row. The row is then appended to the main-container
     
-    const currentRow = document.createElement('div').classList.add('row-container');
+    const currentRow = document.createElement('div');
+    currentRow.classList.add('row-container')
     
-    for (j = 0; j < gridSize; ) {
-        const currentElement = document.createElement('div').classList.add('row-element');
+    for (j = 0; j < gridSize; j++) {
+        const currentElement = document.createElement('div');
+        currentElement.classList.add('row-element');
+        currentElement.innerHTML = externalIMG;
         currentRow.appendChild(currentElement);
     }
 
