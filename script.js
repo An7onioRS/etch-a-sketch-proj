219b28ccb0b5fe47e5a01f8e95145b3dc57ae8cd
Antonio Soprano <antoniosimeonov7@gmail.com>
2022-03-04 09:13:28 +0200

    Added a button to clear the grid and updated each div's event listeners
    When the button is clicked, the program clears the grid and after 1 second
    asks the user for a grid number. The event listeners were updated to
    change their color to a random RGB color and remain with it after the
    mouse leaves the div.

--- script.js
+++ script.js
@@ -1,27 +1,86 @@
 const GRID_SIZE = 16;
 
-let mainContainer = document.createElement('div');
-mainContainer.classList.add('main-container')
-mainContainer.style.width = "960px";
-document.body.appendChild(mainContainer);
+const btnToClearGrid = document.createElement('button');
+btnToClearGrid.innerHTML = "Clear Grid";
+document.body.appendChild(btnToClearGrid);
 
-for (i = 0; i < GRID_SIZE; i++) {
-    // The first loop creates a gridSize number of rows. The second loop creates a gridSize number of elements. The elements are then appended to each row. The row is then appended to the main-container
+let grid = document.createElement('div');
+grid.classList.add('grid')
+grid.style.width = "960px";
+grid.style.border = "10px solid #faed27";
+document.body.appendChild(grid);
+
+fillGrid(GRID_SIZE, grid);
+
+function fillGrid(GRID_SIZE, grid) {
+
+    for (i = 0; i < GRID_SIZE; i++) {
+        // The first loop creates a gridSize number of rows
+        
+        const currentRow = createRowWithClass();
+        
+        for (j = 0; j < GRID_SIZE; j++) {
+            // creates and adds a class to a gridSize number of elements and adds them to the row
+            const currentElement = createElementWitchClass();
+            
+            implementHoverEffect(currentElement);
+
+            currentRow.appendChild(currentElement);
+        }
     
-    const currentRow = document.createElement('tr');
-    currentRow.classList.add('row-container')
+        grid.appendChild(currentRow);
+    };
+}
+
+function implementHoverEffect(element) {
+
+    const randomColor = getRandomColor();
+    element.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = randomColor;
+    });
+    element.addEventListener('mouseleave', (e) => {
+        e.target.style.background = randomColor;
+    });
+}
+
+function createRowWithClass() {
+    const row =  document.createElement('tr');
+    row.classList.add('row-container');
+    return row;
+}
+
+function createElementWitchClass() {
+    const element = document.createElement('td');
+    element.classList.add('row-element');
+    return element;
+}
+
+function clearGrid(gridClassName)
+{
+    document.querySelector(`.${gridClassName}`).innerHTML = "";
+}
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
     
-    for (j = 0; j < GRID_SIZE; j++) {
-        const currentElement = document.createElement('td');
-        currentElement.classList.add('row-element');
-        currentElement.addEventListener('mouseenter', (e) => {
-            e.target.style.backgroundColor = "yellow";
-        })
-        currentElement.addEventListener('mouseleave', (e) => {
-            e.target.style.background = "black";
-        })
-        currentRow.appendChild(currentElement);
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
     }
+    return color;
+}
 
-    mainContainer.appendChild(currentRow);
-};
+btnToClearGrid.addEventListener('click', () => {
+    
+    clearGrid(grid.className);
+
+    setTimeout(() => {
+        let updatedGridSize = "";
+
+        while (updatedGridSize > 100 || updatedGridSize <= 0) {
+        updatedGridSize = parseInt(prompt('The number of the Grid has to be a between 0 and 100! Please select another number!'));
+        }
+
+        fillGrid(updatedGridSize, grid);
+    }, 1000);
+});
